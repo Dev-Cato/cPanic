1fb644955f5ac88ff4a6a6a1bd1ba64373f450cf
CΛTO <118303574+Dev-Cato@users.noreply.github.com>
2022-12-26 12:53:44 +0100

    Update server.lua

--- cPanic/server.lua
+++ cPanic/server.lua
@@ -6,23 +6,23 @@ local _T = Translation[Config.Locale]
 
 --════════════════════════════════════════< script >════════════════════════════════════════--
 ESX.RegisterServerCallback('cPanic:getJob', function(source, cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
 
-    local xJob = xPlayer.job
-    cb(xJob)
+	local xJob = xPlayer.job
+	cb(xJob)
 end)
 
 ESX.RegisterServerCallback('cPanic:hasItem', function(source, cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local Item = xPlayer.getInventoryItem(Config.RequiredItem)
-    
-    cb(Item)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local Item = xPlayer.getInventoryItem(Config.RequiredItem)
+	
+	cb(Item)
 end)
 
 RegisterServerEvent('cPanic:sendServer')
 AddEventHandler('cPanic:sendServer', function(player, officerPed, coords, street)
-    local msg = _T['panicMsg']
-    msg = string.gsub(msg, '_officer_', player)
-    msg = string.gsub(msg, '_street_', street)
-    TriggerClientEvent('cPanic:panic', -1, coords, msg, officerPed)
+	local msg = _T['panicMsg']
+	msg = string.gsub(msg, '_officer_', player)
+	msg = string.gsub(msg, '_street_', street)
+	TriggerClientEvent('cPanic:panic', -1, coords, msg, officerPed)
 end)
