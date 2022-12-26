2f6fcf7ee9c6e8d4549ce16c8f24fb00f2a217cb
CΛTO <118303574+Dev-Cato@users.noreply.github.com>
2022-12-26 12:53:01 +0100

    Update client.lua

--- cPanic/client.lua
+++ cPanic/client.lua
@@ -1,10 +1,10 @@
 --════════════════════════════════════════< esx import >════════════════════════════════════════--
 ESX = nil
 CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Wait(1)
-    end
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Wait(1)
+	end
 end)
 --════════════════════════════════════════< locals >════════════════════════════════════════--
 local _T = Translation[Config.Locale]
@@ -16,55 +16,55 @@ local isPanictune = false
 --════════════════════════════════════════< script >════════════════════════════════════════--
 
 CreateThread(function()
-    Wait(1000)
-    while plyJob == '' do
-        ESX.TriggerServerCallback('cPanic:getJob', function(xJob)
-            plyJob = xJob
-        end)
-        Wait(2000)
-    end
+	Wait(1000)
+	while plyJob == '' do
+		ESX.TriggerServerCallback('cPanic:getJob', function(xJob)
+			plyJob = xJob
+		end)
+		Wait(2000)
+	end
 end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-    plyJob = job
+	plyJob = job
 end)
 
 RegisterCommand(Config.Panictune, function(source, args)
-    if isAllowed() then
-        isPanictune = not isPanictune
-        if isPanictune then
-            Config.Notification(_T['panicJoined'])
-        else
-            Config.Notification(_T['panicLeaved'])
-        end
-    else
-        Config.Notification(_T['notAllowed'])
-    end
+	if isAllowed() then
+		isPanictune = not isPanictune
+		if isPanictune then
+			Config.Notification(_T['panicJoined'])
+		else
+			Config.Notification(_T['panicLeaved'])
+		end
+	else
+		Config.Notification(_T['notAllowed'])
+	end
 end)
 
 RegisterCommand(Config.Panic, function(source, args)
-    if isAllowed() then
-        if isPanictune then
-            local coords = GetEntityCoords(PlayerPedId())
-            local street, cross = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
-            local street = GetStreetNameFromHashKey(street)
+	if isAllowed() then
+		if isPanictune then
+			local coords = GetEntityCoords(PlayerPedId())
+			local street, cross = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
+			local street = GetStreetNameFromHashKey(street)
 
-            TriggerServerEvent('cPanic:sendServer', GetPlayerName(PlayerId()), PlayerPedId(), coords, street)
-        else
-            Config.Notification(_T['notPanictuned'])
-        end
-    else
-        Config.Notification(_T['notAllowed'])
-    end
+			TriggerServerEvent('cPanic:sendServer', GetPlayerName(PlayerId()), PlayerPedId(), coords, street)
+		else
+			Config.Notification(_T['notPanictuned'])
+		end
+	else
+		Config.Notification(_T['notAllowed'])
+	end
 end)
 
 RegisterNetEvent('cPanic:panic')
 AddEventHandler('cPanic:panic', function(coords, msg, officerPed)
-    if isAllowed() then
-        Config.Notification(msg)
+	if isAllowed() then
+		Config.Notification(msg)
 
-        CreateThread(function()
+		CreateThread(function()
 			local blip = AddBlipForRadius(coords, b.radius)
 			SetBlipRoute(blip, true)
 
@@ -73,7 +73,7 @@ AddEventHandler('cPanic:panic', function(coords, msg, officerPed)
 					SetBlipRouteColour(blip, b.color1)
 					Wait(100)
 					SetBlipRouteColour(blip, b.color2)
-                    Wait(100)
+					Wait(100)
 				end
 			end)
 
@@ -85,35 +85,35 @@ AddEventHandler('cPanic:panic', function(coords, msg, officerPed)
 			Wait(Config.PanicTime * 1000)
 
 			RemoveBlip(blip)
-            blip = nil
+			blip = nil
 		end)
 
-    end
+	end
 end)
 
 function isAllowed()
-    local allowed = false
-    if Config.Require == ('job' or 'item-job') then
-        for _, i in pairs(Config.RequiredJobs) do
-            if i == plyJob.name then
-                allowed = true
-            end
-        end
-    end
+	local allowed = false
+	if Config.Require == ('job' or 'item-job') then
+		for _, i in pairs(Config.RequiredJobs) do
+			if i == plyJob.name then
+				allowed = true
+			end
+		end
+	end
 
-    if Config.Require == ('item' or 'item-job') then
-        ESX.TriggerServerCallback('cPanic:hasItem', function(xItem)
-            print(xItem.count)
-            if xItem.count > 0 then
-                allowed = true
-                print("allow")
-            end
-        end)
-    end
+	if Config.Require == ('item' or 'item-job') then
+		ESX.TriggerServerCallback('cPanic:hasItem', function(xItem)
+			print(xItem.count)
+			if xItem.count > 0 then
+				allowed = true
+				print("allow")
+			end
+		end)
+	end
 
-    if Config.Require == 'none' then
-        allowed = true
-    end
-    Wait(100)
-    return allowed
+	if Config.Require == 'none' then
+		allowed = true
+	end
+	Wait(100)
+	return allowed
 end
