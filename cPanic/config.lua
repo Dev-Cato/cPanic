33071b30f12624bd67f8234abb5dbc4527c33a37
CΛTO <118303574+Dev-Cato@users.noreply.github.com>
2022-12-26 12:53:29 +0100

    Update config.lua

--- cPanic/config.lua
+++ cPanic/config.lua
@@ -12,36 +12,36 @@ Config.RequiredItem = 'panic' -- item that is required to use panictune and pani
 
 
 Config.Notification = function(msg) -- put here in your notify trigger
-    ESX.ShowNotification(msg)
+	ESX.ShowNotification(msg)
 end
 
 Config.Blip = { -- you can customize the blip here
-    radius = 100.0,
-    color1 = 1, color2 = 35,
-    alpha = 60,
+	radius = 100.0,
+	color1 = 1, color2 = 35,
+	alpha = 60,
 }
 
 Config.Locale = 'de' -- 'de', 'en' or 'custom'
 Translation = {
-    ['de'] = {
-        ['notAllowed'] = '~r~Dazu bist du nicht befugt!',
-        ['notPanictuned'] = '~r~Du bist nicht im Panictune!',
-        ['panicJoined'] = '~b~Du bist nun im Panictune',
-        ['panicLeaved'] = '~b~Du bist nun nicht mehr im Panictune',
-        ['panicMsg'] = 'Der Officer ~b~_officer_~s~, hat ein Panicbutton in der ~b~_street_~s~ gedrückt!'
-    },
-    ['en'] = {
-        ['notAllowed'] = '~r~You are not authorized to do this!',
-        ['notPanictuned'] = '~r~You\'re not in Panictune!',
-        ['panicJoined'] = '~b~You are now in Panictune',
-        ['panicLeaved'] = '~b~You are no longer in Panictune',
-        ['panicMsg'] = 'Officer ~b~_officer_~s~ pressed a panic button on ~b~_street_~s~!'
-    },
-    ['custom'] = {
-        ['notAllowed'] = '',
-        ['notPanictuned'] = '',
-        ['panicJoined'] = '',
-        ['panicLeaved'] = '',
-        ['panicMsg'] = ''
-    },
+	['de'] = {
+		['notAllowed'] = '~r~Dazu bist du nicht befugt!',
+		['notPanictuned'] = '~r~Du bist nicht im Panictune!',
+		['panicJoined'] = '~b~Du bist nun im Panictune',
+		['panicLeaved'] = '~b~Du bist nun nicht mehr im Panictune',
+		['panicMsg'] = 'Der Officer ~b~_officer_~s~, hat ein Panicbutton in der ~b~_street_~s~ gedrückt!'
+	},
+	['en'] = {
+		['notAllowed'] = '~r~You are not authorized to do this!',
+		['notPanictuned'] = '~r~You\'re not in Panictune!',
+		['panicJoined'] = '~b~You are now in Panictune',
+		['panicLeaved'] = '~b~You are no longer in Panictune',
+		['panicMsg'] = 'Officer ~b~_officer_~s~ pressed a panic button on ~b~_street_~s~!'
+	},
+	['custom'] = {
+		['notAllowed'] = '',
+		['notPanictuned'] = '',
+		['panicJoined'] = '',
+		['panicLeaved'] = '',
+		['panicMsg'] = ''
+	},
 }
